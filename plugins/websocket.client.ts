f1b3a775d2d29a2b5c3d862226d6d67da237d5c0
kgarchie <archiethebig@gmail.com>
2023-05-04 15:53:30 +0300

    Bug fix - Prisma Generate now on build

--- plugins/websocket.client.ts
+++ plugins/websocket.client.ts
@@ -16,10 +16,10 @@ const wsProtocol = window.location.protocol === "https:" ? "wss:" : "ws:"
 
 // For Development
 const domain = window.location.hostname
-const socket_url = `${wsProtocol}//${domain}:${websocketPort}`
+// const socket_url = `${wsProtocol}//${domain}:${websocketPort}`
 
 // For Production
-// const socket_url:string = `${wsProtocol}//${window.location.host}`
+const socket_url:string = `${wsProtocol}//${window.location.host}`
 
 export default defineNuxtPlugin(() => {
     const newMessageState = useNewMessage()
