b8be1f9b0da315108521c60e4790f33794298b00
kgarchie <archiethebig@gmail.com>
2023-05-17 21:42:36 +0300

    Trial Fix For File Upload To Public

--- mvc/chats/helpers.ts
+++ mvc/chats/helpers.ts
@@ -1,14 +1,31 @@
 import * as fs from "fs";
+import path from "path";
+
 export function obtainChat_id(from_user_id: string, to_user_id: string) {
     const users = [from_user_id, to_user_id].sort((a, b) => a.localeCompare(b))
     return users[0] + users[1]
 }
 
 
-export function writeFileToStorage(path: string, locationsOnDisk: string[], file:any) {
+export function writeFileToStorage(given_path: string, locationsOnDisk: string[], file:any) {
     // TODO: Find a better way of serving static files in nuxt3 instead of using public folder
-    let root = `public/uploads/${path}`
-    let dir = `uploads/${path}`
+    // let root = `/public/uploads/${path}`
+    let dirName = path.resolve(path.dirname(''))
+    let cwd = process.cwd()
+    // console.log("cwd: ", cwd)
+
+    while (!dirName.endsWith("public")) {
+        dirName = dirName.substring(0, dirName.lastIndexOf("\\"))
+
+        if (dirName.length === 0) {
+            dirName = cwd
+            dirName += "/public"
+            break
+        }
+    }
+
+    let root = `${dirName}/uploads/${given_path}`
+    let dir = `uploads/${given_path}`
 
     if (!fs.existsSync(root)) {
         fs.mkdirSync(root, {recursive: true})
